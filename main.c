32439899541ba7b52a9db7953169daeda1fcd16e
Fernando Robles <80862945+ferroblesmdq12@users.noreply.github.com>
2021-10-10 13:53:11 -0300

    Primera version
    Iniciando los primeros pasos de presentación del programa

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <strings.h>
+#include <conio.h>
+#include "gotoxy.h"
+
+
+int main()
+{
+    color(10);
+    printf("\t\t ARBOLES!\n");
+    system("pause");
+    system("cls");
+    color(23);
+    printf("\t Realizado por Fernando Robles \n");
+    system("pause");
+    system("cls");
+    color(1);
+
+    printf("\t\t\t\t ╔══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("\t\t\t\t ║                              FUNCIONES                                       ║\n");
+    printf("\t\t\t\t ║                                                                              ║\n");
+    printf("\t\t\t\t ╚══════════════════════════════════════════════════════════════════════════════╝\n");
+
+    return 0;
+}
