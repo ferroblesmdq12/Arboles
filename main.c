81f6bcf2c81395a4a74a6fec90af7944d04a932f
Fernando Robles <80862945+ferroblesmdq12@users.noreply.github.com>
2021-10-10 21:12:53 -0300

    Codigo de arbol funcionando
    funcionamiento perfecto...

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 #include "gotoxy.h"
 
 
-struct nodoArbol
+typedef struct nodoArbol
 {
     int dato;
-    nodoArbol *izq;
-    nodoArbol *der;
-};
+    struct nodoArbol *izq;
+    struct nodoArbol *der;
+}nodoArbol;
 
 /// PROTOTIPADO ///
-nodoArbol * inicArbol();
-nodoArbol * crearNodoArbol(int dato);
-nodoArbol * insertar(nodoArbol * arbol, int dato);
+nodoArbol* inicArbol();
+nodoArbol* crearNodoArbol(int dato);
+nodoArbol* insertar(nodoArbol * arbol, int dato);
+void preOrder(nodoArbol * arbol);
+void inOrder(nodoArbol * arbol);
+void postorder(nodoArbol * arbol);
+nodoArbol * buscar(nodoArbol * arbol, int dato);
 
 
 int main()
@@ -25,19 +29,36 @@ int main()
     printf("\t\t ARBOLES!\n");
     system("pause");
     system("cls");
+
     color(23);
     printf("\t Realizado por Fernando Robles \n");
     system("pause");
     system("cls");
+
     color(1);
 
-    printf("\t\t\t\t ╔══════════════════════════════════════════════════════════════════════════════╗\n");
-    printf("\t\t\t\t ║                              FUNCIONES                                       ║\n");
-    printf("\t\t\t\t ║                                                                              ║\n");
-    printf("\t\t\t\t ╚══════════════════════════════════════════════════════════════════════════════╝\n");
-    printf("\n Hacer algo ...");
+    printf("\t\t\t\t  ______________________________________________________________________________\n");
+    printf("\t\t\t\t |                              FUNCIONES                                       |\n");
+    printf("\t\t\t\t |                                                                              |\n");
+    printf("\t\t\t\t |______________________________________________________________________________|\n");
 
+    printf("\n Hacer algo ...\n");
 
+    int a[10] = {1,10,2,45,9,15,46,33,25,20};
+    nodoArbol * arbol = inicArbol();
+    for( int i=0; i<10; i++)
+    {
+        arbol = insertar(arbol, a[i]);
+    }
+
+    printf("\n\n PREORDER: \n");
+    preOrder(arbol);
+
+    printf("\n\n INORDER: \n");
+    inOrder(arbol);
+
+    printf("\n\n POSTORDER \n");
+    postorder(arbol);
 
     return 0;
 }
@@ -82,3 +103,64 @@ nodoArbol * insertar(nodoArbol * arbol, int dato)
 }
 
 
+/// PREORDER ///
+
+void preOrder(nodoArbol * arbol)
+{
+    if(arbol!= NULL)
+    {
+        printf("\n %d", arbol->dato);
+        preOrder(arbol->izq);
+        preOrder(arbol->der);
+    }
+}
+
+/// INORDER ///
+
+void inOrder(nodoArbol * arbol)
+{
+    if(arbol != NULL)
+    {
+        inOrder(arbol->izq);
+        printf("\n %d", arbol->dato);
+        inOrder(arbol->der);
+    }
+}
+
+/// POSTORDER ///
+
+void postorder(nodoArbol * arbol)
+{
+    if(arbol != NULL)
+    {
+        postorder(arbol->izq);
+        postorder(arbol->der);
+        printf("\n %d", arbol->dato);
+    }
+}
+
+/// Buscar NODO ///
+
+nodoArbol * buscar(nodoArbol * arbol, int dato)
+{
+    nodoArbol * rta = NULL;
+    if(arbol!=NULL)
+    {
+        if(dato == arbol->dato)
+        {
+            rta = arbol;
+        }
+        else
+        {
+            if(dato == arbol->dato)
+            {
+                rta = buscar(arbol->der, dato);
+            }
+            else
+            {
+                rta = buscar(arbol->izq, dato);
+            }
+        }
+    }
+    return rta;
+}
